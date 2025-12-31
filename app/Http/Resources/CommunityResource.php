95bde56ffb7bb32cd2337f856d061406dc1fe0ce
ezechieldef <ezechielahouandjinou@gmail.com>
2025-12-31 08:58:38 +0100

    ...

--- app/Http/Resources/CommunityResource.php
+++ app/Http/Resources/CommunityResource.php
@@ -2,8 +2,10 @@
 
 namespace App\Http\Resources;
 
+use App\Models\CommunityMembership;
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Http\Resources\Json\JsonResource;
 
 class CommunityResource extends JsonResource
@@ -24,6 +26,24 @@ class CommunityResource extends JsonResource
             $permissions = User::find($user_id)?->communityPermissions($community_id)->pluck('name')->toArray() ?? null;
             $data['permissions'] = $permissions ?? [];
             unset($data['pivot']);
+        } else {
+            $user_id = Auth::user()?->id ?? null;
+            $community_id = $this->id;
+            $membership = CommunityMembership::where('user_id', $user_id)
+                ->where('community_id', $community_id)
+                ->first();
+            if ($user_id && $membership) {
+                $data['role'] = $membership->role ?? null;
+                $oldTeamId = getPermissionsTeamId();
+                setPermissionsTeamId($community_id);
+                $permissions = User::find($user_id)?->communityPermissions($community_id)->pluck('name')->toArray() ?? null;
+
+                $data['permissions'] = $permissions ?? [];
+                setPermissionsTeamId($oldTeamId);
+            } else {
+                $data['role'] = null;
+                $data['permissions'] = [];
+            }
         }
         $this->loadMissing('country');
         $data['country'] = CountryResource::make($this->whenLoaded('country'));
